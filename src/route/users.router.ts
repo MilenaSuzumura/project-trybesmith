4f109486b6afb3cbd93c4c11195b3ca1bf6b20ca
MilenaSuzumura <milena.suzumura11@gmail.com>
2022-11-23 15:21:46 -0300

    requisito 7 feito

--- src/route/users.router.ts
+++ src/route/users.router.ts
@@ -1,9 +1,14 @@
 import { Router } from 'express';
 import UsersControllers from '../controllers/users.controllers';
+import validation from '../validation/validation';
 
 const usersRoute = Router();
 const usersControllers = new UsersControllers();
 
-usersRoute.post('/', usersControllers.createUser.bind(usersControllers));
+usersRoute.post(
+  '/',
+  validation.validationUser,
+  usersControllers.createUser.bind(usersControllers),
+);
 
 export default usersRoute;
--- src/validation/validation.ts
+++ src/validation/validation.ts
@@ -15,6 +15,30 @@ const productValidation = Joi.object({
   }),
 });
 
+const userValidation = Joi.object({
+  username: Joi.string().min(3).required()
+    .messages({
+      'string.base': '"username" must be a string',
+      'string.min': '"username" length must be at least 3 characters long',
+      'any.required': '"username" is required',
+    }),
+  classe: Joi.string().min(3).required().messages({
+    'string.base': '"classe" must be a string',
+    'string.min': '"classe" length must be at least 3 characters long',
+    'any.required': '"classe" is required',
+  }),
+  level: Joi.number().min(1).required().messages({
+    'number.base': '"level" must be a number',
+    'number.min': '"level" must be greater than or equal to 1',
+    'any.required': '"level" is required',
+  }),
+  password: Joi.string().min(8).required().messages({
+    'string.base': '"password" must be a string',
+    'string.min': '"password" length must be at least 8 characters long',
+    'any.required': '"password" is required',
+  }),
+});
+
 const validationProduct = (req: Request, res: Response, next: NextFunction) => {
   const { error } = productValidation.validate(req.body);
   if (error?.message === '"name" is required' || error?.message === '"amount" is required') {
@@ -28,6 +52,20 @@ const validationProduct = (req: Request, res: Response, next: NextFunction) => {
   next();
 };
 
+const validationUser = (req: Request, res: Response, next: NextFunction) => {
+  const { error } = userValidation.validate(req.body);
+  if (error?.details[0].type === 'any.required') {
+    return res.status(400).json({ message: error.message });
+  }
+
+  if (error?.message) {
+    return res.status(422).json({ message: error.message });
+  }
+
+  next();
+};
+
 export default {
   validationProduct,
+  validationUser,
 };
