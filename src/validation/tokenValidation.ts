61af1e358dd10ea77fde142e4b347f76e8708ef9
MilenaSuzumura <milena.suzumura11@gmail.com>
2022-11-24 20:22:15 -0300

    validação do token invalido

--- src/validation/tokenValidation.ts
+++ src/validation/tokenValidation.ts
@@ -1,11 +1,20 @@
+import jwt from 'jsonwebtoken';
 import { Request, Response, NextFunction } from 'express';
+// import UsersModel from '../models/users.model';
 
 const verificacaoToken = (req: Request, res: Response, next: NextFunction) => {
+  // const usersModel = new UsersModel();
   const { authorization } = req.headers;
-
+  
   if (!authorization) {
     return res.status(401).json({ message: 'Token not found' });
   }
+  
+  jwt.verify(authorization, String(process.env.JWT_SECRET), (err, _decoded) => {
+    if (err) {
+      return res.status(401).json({ message: 'Invalid token' });
+    }
+  });
 
   next();
 };
